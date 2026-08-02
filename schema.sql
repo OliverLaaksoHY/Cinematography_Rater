26c288971a2a90dff862d53ead9965e6b82cef36
Oliver <oliver.laakso@helsinki.fi>
2026-08-02 06:47:57 +0300

    added items table to sql schema

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    title TEXT UNIQUE,
+    description TEXT,
+    focal_length INTEGER,
+    geolocation TEXT,
+    user_id INTEGER REFERENCES users
+);
