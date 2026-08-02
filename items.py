6ec883efa304c2a0496459a8e869e2541b70094e
Oliver <oliver.laakso@helsinki.fi>
2026-08-02 07:46:24 +0300

    add function to retreive all values for a specific item

--- items.py
+++ items.py
@@ -7,4 +7,19 @@ def add_item(title, description, focal_length, location, user_id):
         (?, ?, ?, ?, ?)
         """
     db.execute(sql, [title, description, focal_length, location, user_id])
-    
+
+def get_items():
+    sql = """
+        SELECT id, title FROM items ORDER BY id DESC
+    """
+    return db.query(sql)
+
+
+def get_item(item_id):
+    sql = """
+        SELECT I.title, I.description, I.focal_length, I.geolocation, U.username, user_id 
+        FROM items I, users U 
+        WHERE I.user_id = U.id
+        AND U.id = ?"""
+    result = db.query(sql, [item_id])[0]
+    return result
