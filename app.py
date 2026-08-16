8efd68532ee1e020e6551efb2ac574b82e979293
Oliver <oliver.laakso@helsinki.fi>
2026-08-16 23:16:51 +0300

    Refactor regirstration and login

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
 from flask import Flask
 from flask import abort, redirect, render_template, request, session
-from werkzeug.security import generate_password_hash, check_password_hash
 import db
 import items
 import users
@@ -40,7 +39,7 @@ def show_user(user_id):
     if not user:
         abort(403)
     items = users.get_user_posts(user_id)
-    print(items[0])
+    
     return render_template("show_user.html", user=user, items=items)
 
 
@@ -159,15 +158,14 @@ def create():
     password2 = request.form["password2"]
     if password1 != password2:
         return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
 
     try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
+        users.create_user(username, password1)
     except sqlite3.IntegrityError:
         return "VIRHE: tunnus on jo varattu"
-
+    
     return "Tunnus luotu"
+    
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -177,13 +175,8 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         
-        sql = "SELECT id, password_hash FROM users WHERE username = ?"
-        
-        result = db.query(sql, [username])[0]
-        password_hash = result["password_hash"]
-        user_id = result["id"]
-
-        if check_password_hash(password_hash, password):
+        user_id = users.check_login(username, password)
+        if user_id:    
             session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
