fb39aa1b3a0692f5bbf3f818197b896ff521bdec
Oliver <oliver.laakso@helsinki.fi>
2026-08-30 07:18:09 +0300

    add database indexes

--- schema.sql
+++ schema.sql
@@ -33,3 +33,37 @@ CREATE TABLE image_classes (
     title TEXT,
     value TEXT 
 );
+
+
+CREATE UNIQUE INDEX idx_users_id
+ON users(id);
+
+CREATE UNIQUE INDEX idx_users_username
+ON users(username);
+
+CREATE UNIQUE INDEX idx_images_id
+ON images(id);
+
+CREATE UNIQUE INDEX idx_images_title
+ON images(title);
+
+CREATE UNIQUE INDEX idx_reviews_id
+ON reviews(id);
+
+CREATE UNIQUE INDEX idx_classes_id
+ON classes(id);
+
+CREATE UNIQUE INDEX idx_image_classes_id
+ON image_classes(id);
+
+CREATE INDEX idx_images_user_id
+ON images(user_id);
+
+CREATE INDEX idx_reviews_image_id
+ON reviews(image_id);
+
+CREATE INDEX idx_reviews_user_id
+ON reviews(user_id);
+
+CREATE INDEX idx_image_classes_image_id
+ON image_classes(image_id);
